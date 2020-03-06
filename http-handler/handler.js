834f20c735449e817afa9d3802f4f58a580daee6
Fabricio Teixeira Nascimento <fabricio.nascimento@bancointer.com.br>
2020-03-06 17:39:44 -0300

    Initial commit

---
+++ http-handler/handler.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const s3Service = require('./service/s3service')
+
+module.exports.upload = async event => {
+  const result = await s3Service.upload(event.body)
+  return {
+    statusCode: 201,
+    body: JSON.stringify(result)
+  };
+};

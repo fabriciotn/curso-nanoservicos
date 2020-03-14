9adc3d4fb4af4da96a5a4b900ec7a3ebba79e877
Fabricio Teixeira Nascimento <fabriciotn82@gmail.com>
2020-03-14 16:50:27 -0300

    Funcionalidade para salvar dados no DynamoDb

--- http-handler/handler.js
+++ http-handler/handler.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const s3Service = require('./service/s3service')
+const dynamoDbService = require('./service/dynamoDbServices')
 
 module.exports.upload = async event => {
-  const result = await s3Service.upload(event.body)
+  const item = await s3Service.upload(event.body);
+  await dynamoDbService.put(item);
   return {
     statusCode: 201,
-    body: JSON.stringify(result)
+    body: JSON.stringify(item)
   };
 };

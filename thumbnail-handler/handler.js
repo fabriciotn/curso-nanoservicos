5484fd339fd0b747581ce39551fe8654fbb0bb7c
Fabricio Teixeira Nascimento <fabricio.nascimento@bancointer.com.br>
2020-03-21 12:36:52 -0300

    Implementação da função de Thumbnail.

--- thumbnail-handler/handler.js
+++ thumbnail-handler/handler.js
@@ -1,19 +1,9 @@
 'use strict';
 
-module.exports.thumbnail = async event => {
-  console.log('Evento do SNS recebido com sucesso', JSON.stringify(event));
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const thumbnailService = require('./service/thumbnailService')
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.thumbnail = async event => {
+  console.log('Evento do SNS recebido com sucesso', JSON.stringify(event))
+  await thumbnailService.thumbnail(event)
+  return { message: 'Thumbnail gerado com sucesso!', event };
 };

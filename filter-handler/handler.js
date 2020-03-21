fc2025456fd276cfa5dbfdc160ae49604d4751a9
Fabricio Teixeira Nascimento <fabriciotn82@gmail.com>
2020-03-21 15:08:06 -0300

    Implementação do Filter Handler

--- filter-handler/handler.js
+++ filter-handler/handler.js
@@ -1,18 +1,9 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const filterService = require('./service/filterService')
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.filter = async event => {
+  console.log('Evento do SNS recebido com sucesso', JSON.stringify(event))
+  await filterService.filter(event)
+  return { message: 'Filtro gerado com sucesso!', event };
 };
